23bdcb405c6ce54c4f6ed394934d418b87167687
wilberforce001 <kenmisik@gmail.com>
2023-08-01 10:38:24 +0300

    Initialized Game variables

--- main.js
+++ main.js
@@ -8,5 +8,24 @@ form.addEventListener("submit", (event) => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
     document.querySelector(".modal-wrapper").setAttribute("hidden", true);
+    InitializeGame(data);
+});
+
+const InitializeVariables = (data) => {
+    data.choice = +data.choice;
+    data.board = [0, 1, 2, 3, 4, 5, , 7, 8];
+    data.player1 = "X"
+    data.player2 = "O"
+    data.round = 0;
+    data.curentPlayer = "X";
+    data.gameOver = false;
+}
+
+const InitializeGame = (data) => {
+    // Initialize game variables
+    InitializeVariables(data);
+
     console.log(data);
-})
+    // Add event listeners to the gameboard
+    
+}
